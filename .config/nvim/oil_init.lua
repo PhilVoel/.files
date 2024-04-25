53d734e8ca65e3fc3a7148492c3c7dca4481c9c0
PhilVoel <philipp123.voeller@gmail.com>
2024-04-25 21:09:59 +0200

    Updated oil.nvim config

--- .config/nvim/oil_init.lua
+++ .config/nvim/oil_init.lua
@@ -34,7 +34,8 @@ vim.api.nvim_create_autocmd("User", {
 	callback = vim.schedule_wrap(function(args)
 		local oil = require("oil")
 		if vim.api.nvim_get_current_buf() == args.data.buf and oil.get_cursor_entry() then
-			oil.select({ preview = true })
+			oil.open_preview()
+			-- oil.select({ preview = true })
 		end
 		-- vim.cmd("normal jW")
 	end),
