3b9400f2f6e1e73669135730d310a199832f0b6c
PhilVoel <philipp123.voeller@gmail.com>
2024-04-25 17:17:53 +0200

    Added vim apm

---
+++ .config/nvim/lua/plugins/apm.lua
@@ -0,0 +1,8 @@
+return {
+	"theprimeagen/vim-apm",
+	config = function()
+		local apm = require("vim-apm")
+		apm:setup({})
+		vim.keymap.set("n", "<leader>apm", function() apm:toggle_monitor() end, {desc = "Toggle APM monitor"})
+	end
+}
